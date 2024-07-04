8156f0bf209fa4c16eb5d8c202010968c374a2a4
Ramazan  TOY <ramcotoy06@gmail.com>
2024-07-04 18:01:11 +0300

    frog tongue summary added

--- Assets/Scripts/FrogScripts/Tongue/FrogTongue.cs
+++ Assets/Scripts/FrogScripts/Tongue/FrogTongue.cs
@@ -8,75 +8,81 @@ using UnityEngine;
 
 namespace FrogScripts.Tongue
 {
+    /// <summary>
+    /// This class effectively manages the frog's tongue behavior, including its states (idle, extending, retracting), movement, and interactions with hex tiles and views.
+    /// </summary>
     public class FrogTongue : MonoBehaviour
     {
-
         private Frog _myFrog;
 
         public Frog Frog
         {
-            set
-            {
-                _myFrog = value;
-            }
+            set { _myFrog = value; }
         }
+
         private LineRenderer _lineRenderer;
 
-        [SerializeField]
-        private GameObject _tonguePointPref;
+        [SerializeField] private GameObject _tonguePointPref;
 
         private List<GameObject> _availablePoints = new List<GameObject>();
-        [SerializeField]
-        private List<GameObject> _usedPoints;
+
+        [SerializeField] private List<GameObject> _usedPoints;
 
         private List<HexView> _visitedHexViews = new List<HexView>();
         private List<HexTile> _visitedTiles = new List<HexTile>();
 
 
-
+        /// <summary>
+        /// Adds a hex view to the visited list if not already added.
+        /// </summary>
         public void AddViewVisited()
         {
             var hexTile = GetTargetTile();
-            
-            if(hexTile==null) return;
-            
-            if(_visitedTiles.Contains(hexTile)) return;
+
+            if (hexTile == null) return;
+
+            if (_visitedTiles.Contains(hexTile)) return;
             _visitedTiles.Add(hexTile);
-            
+
             var hexView = hexTile.GetTopStackElement();
-            if(_visitedHexViews.Contains(hexView)) return;
+            if (_visitedHexViews.Contains(hexView)) return;
             _visitedHexViews.Add(hexView);
         }
 
+        /// <summary>
+        /// Retrieves the last visited hex view.
+        /// </summary>
+        /// <returns></returns>
         public HexView GetLastVisitedView()
         {
             if (_visitedHexViews.Count <= 0) return null;
-            
+
             var view = _visitedHexViews[^1];
-            
-            _visitedHexViews.RemoveAt(_visitedHexViews.Count-1);
+
+            _visitedHexViews.RemoveAt(_visitedHexViews.Count - 1);
             return view;
         }
 
 
-
         public Vector3 TargetPoint { get; set; }
 
+        /// <summary>
+        /// Updated line renderer every frame
+        /// </summary>
         private void FixedUpdate()
         {
             _lineRenderer.positionCount = _usedPoints.Count;
-            
+
             for (var i = 0; i < _usedPoints.Count; i++)
             {
                 _lineRenderer.SetPosition(i, _usedPoints[i].transform.position);
             }
         }
 
-        [SerializeField]
-        private TongueState _tongueState;
-        
+        [SerializeField] private TongueState _tongueState;
+
         public TongueState TongueState => _tongueState;
-        
+
         private TongueStateMachine _currentStateMachine;
         private TongueIdleStateMachine _tongueIdleStateMachine;
         private TongueExtendingStateMachine _tongueExtendingStateMachine;
@@ -89,19 +95,23 @@ namespace FrogScripts.Tongue
         private Direction _movementDirection;
 
         public bool IsMovementSuccessfullyCompleted { private set; get; }
-        
-        #endregion
-        
-        
 
+        #endregion
+
+
+        /// <summary>
+        /// Initializes state machines.
+        /// </summary>
         private void Init()
         {
             _tongueIdleStateMachine = new TongueIdleStateMachine(this);
             _tongueExtendingStateMachine = new TongueExtendingStateMachine(this);
             _tongueRetractingStateMachine = new TongueRetractingStateMachine(this);
-            
         }
-        
+
+        /// <summary>
+        ///  Initializes components and sets the tongue state to idle.
+        /// </summary>
         private void Start()
         {
             _lineRenderer = transform.GetComponent<LineRenderer>();
@@ -110,24 +120,29 @@ namespace FrogScripts.Tongue
             SetTongueState(TongueState.Idle);
         }
 
+        /// <summary>
+        /// Changes the tongue state and handles the state transition.
+        /// </summary>
+        /// <param name="tongueState"></param>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
         private void SetTongueState(TongueState tongueState)
         {
-            if(tongueState==_tongueState) return;
+            if (tongueState == _tongueState) return;
 
             _tongueState = tongueState;
-            
+
             switch (tongueState)
             {
-                case TongueState.Idle :
+                case TongueState.Idle:
                     _currentStateMachine = _tongueIdleStateMachine;
                     _currentStateMachine.OnEnter();
                     break;
-                case TongueState.Retracting :
+                case TongueState.Retracting:
                     _myFrog.OnRetracting();
                     _currentStateMachine = _tongueRetractingStateMachine;
                     _currentStateMachine.OnEnter();
                     break;
-                case TongueState.Extending :
+                case TongueState.Extending:
                     _currentStateMachine = _tongueExtendingStateMachine;
                     _currentStateMachine.OnEnter();
                     break;
@@ -137,23 +152,34 @@ namespace FrogScripts.Tongue
             }
         }
 
-        public void StartExtending(Vector2Int currentCoordinate, Direction direction,ColorType targetColorType)
+        /// <summary>
+        /// Starts the tongue extending process.
+        /// </summary>
+        /// <param name="currentCoordinate"></param>
+        /// <param name="direction"></param>
+        /// <param name="targetColorType"></param>
+        public void StartExtending(Vector2Int currentCoordinate, Direction direction, ColorType targetColorType)
         {
             TargetColorType = targetColorType;
             _movementDirection = direction;
             _targetCoordinate = currentCoordinate.GetCoordinate(direction);
-            
+
             var targetTile = GetTargetTile();
-            
+
             if (targetTile == null)
             {
-              SetTongueState(TongueState.Idle);
+                SetTongueState(TongueState.Idle);
             }
+
             var targetTilePos = targetTile.transform.position;
             TargetPoint = new Vector3(targetTilePos.x, targetTilePos.y, -.25f);
             SetTongueState(TongueState.Extending);
         }
 
+        /// <summary>
+        /// Gets the current hex view from the target tile.
+        /// </summary>
+        /// <returns></returns>
         public HexView GetCurrentHexView()
         {
             var tile = GetTargetTile();
@@ -161,13 +187,15 @@ namespace FrogScripts.Tongue
             return tile == null ? null : tile.GetTopStackElement();
         }
 
+        /// <summary>
+        /// Handles the steps during tongue extension.
+        /// </summary>
         public void OnExtendingStep()
         {
-            
             _targetCoordinate = _targetCoordinate.GetCoordinate(_movementDirection);
-            
+
             var targetTile = GetTargetTile();
-            
+
             if (targetTile == null)
             {
                 IsMovementSuccessfullyCompleted = true;
@@ -177,16 +205,23 @@ namespace FrogScripts.Tongue
 
             var targetTilePos = targetTile.transform.position;
             TargetPoint = new Vector3(targetTilePos.x, targetTilePos.y, -.25f);
-            
+
             _currentStateMachine.OnEnter();
         }
 
+        /// <summary>
+        /// Retrieves the target hex tile.
+        /// </summary>
+        /// <returns></returns>
         private HexTile GetTargetTile()
         {
-           return GameFuncs.GetTile(_targetCoordinate);
+            return GameFuncs.GetTile(_targetCoordinate);
         }
-        
 
+        /// <summary>
+        /// Retrieves or creates a tongue point.
+        /// </summary>
+        /// <returns></returns>
         public GameObject GetPoint()
         {
             if (_availablePoints.Count > 0)
@@ -204,54 +239,76 @@ namespace FrogScripts.Tongue
             return newPoint;
         }
 
+        /// <summary>
+        /// Gets the position of the last tongue point.
+        /// </summary>
         private Vector3 GetLastPoint => _usedPoints[^1].gameObject.transform.position;
 
+        /// <summary>
+        /// Gets the list of used points.
+        /// </summary>
         public List<GameObject> GetUsedPoints => _usedPoints;
 
+        /// <summary>
+        /// Handles actions after the tongue retracting is done.
+        /// </summary>
         public void OnRetractingDone()
         {
             if (IsMovementSuccessfullyCompleted)
             {
                 BlowYourSelf();
             }
+
             ResetPoints();
             SetTongueState(TongueState.Idle);
         }
 
+        /// <summary>
+        /// Blows the top element of each visited tile and notifies the frog of movement completion.
+        /// </summary>
         private void BlowYourSelf()
         {
             foreach (var hexTile in _visitedTiles)
             {
                 hexTile.BlowTopElement().Forget();
             }
+
             _myFrog.OnMovementDone();
         }
 
+        /// <summary>
+        /// Resets the tongue points for reuse.
+        /// </summary>
         private void ResetPoints()
         {
             var point = _usedPoints[0];
             _usedPoints.RemoveAt(0);
-            
+
             foreach (var usedPoint in _usedPoints)
             {
                 _availablePoints.Add(usedPoint);
             }
+
             _usedPoints.Clear();
             _usedPoints.Add(point);
         }
 
+        /// <summary>
+        /// Handles failure during tongue extension.
+        /// </summary>
         public void OnExtendingFail()
         {
             IsMovementSuccessfullyCompleted = false;
             SetTongueState(TongueState.Retracting);
         }
 
+        /// <summary>
+        /// Changes the movement direction.
+        /// </summary>
+        /// <param name="newDirection"></param>
         public void OnMovementDirectionChanged(Direction newDirection)
         {
             _movementDirection = newDirection;
         }
-
-
-
     }
 }
